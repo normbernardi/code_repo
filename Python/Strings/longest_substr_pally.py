8b99335b29c00bb253bf3df3c809f33a34eb9181
Norman <bernardinorman@gmail.com>
2022-05-27 19:50:06 -0400

    Updated code on Longest Substring Palindrome

--- Python/Strings/longest_substr_pally.py
+++ Python/Strings/longest_substr_pally.py
@@ -5,11 +5,12 @@ def longest_substr_pally(s: str) -> str:
         return s
     pally = []
     for i in range(0, len(s)):
-        test = []
-        for j in range(i+1, len(s)):
-            if is_palindrome(s[i:j]):
+        test = [s[i]]
+        for j in range(i+1, len(s)+1):
+            if not is_palindrome(s[i:j]):
+                j += 1
+            elif len(s[i:j]) > len(test):
                 test = [x for x in s[i:j]]
-            j += 1
         if len(pally) < len(test):
             pally = test
     return "".join(pally)
@@ -23,4 +24,4 @@ def is_palindrome(s: str) -> bool:
     return is_palindrome(s[1:-1])
 
 
-print(longest_substr_pally(''))
+print(longest_substr_pally('babad'))
